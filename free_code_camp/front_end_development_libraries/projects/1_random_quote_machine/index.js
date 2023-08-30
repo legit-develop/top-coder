8d09ccbf54cfb692e44ebe1976d1ed1ae3cb8bad
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-08-30 13:48:17 +0200

    set up the asyn function to work on the file

--- free_code_camp/front_end_development_libraries/projects/1_random_quote_machine/index.js
+++ free_code_camp/front_end_development_libraries/projects/1_random_quote_machine/index.js
@@ -1,5 +1,22 @@
 function App() {
-  return <div>Hello World</div>;
+  const [quotes, setQuotes] = React.useState([]);
+  const [randomQuotes, setRandomQuotes] = React.useState([]);
+  const apiURL = "https://api.quotable.io/random";
+  React.useEffect(() => {
+    async function getQuote() {
+      const response = await fetch(apiURL);
+      const data = await response.json();
+      setQuotes(data);
+      setRandomQuotes(data);
+    }
+    getQuote();
+  }, []);
+  return (
+    <div className="text-center">
+      <h3>{quotes.content}</h3>
+      <h6>{quotes.author}</h6>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById("app"));
